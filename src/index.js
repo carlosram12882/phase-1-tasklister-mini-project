cc947306889d8b0f5e8751e05303961ec7eff1ae
Carlos Ramirez <carlosramirez686@gmail.com>
2022-01-21 13:36:31 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+//Gets Tasks List 
+const getsList = function (){
+   return document.getElementById("tasks")
+}
+//Makes new task
+let newTask = function (input){
+  //Creating the new element
+  let newElement = document.createElement("il")//.appendChild('il').innerText(`${imput}`)
+  let ModElement = newElement.getsList().append()
+  return ModElement.innerText(`${input}`)
+  //return getsList().appendChild('il')
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //document.addEventListener("click", )
 });
